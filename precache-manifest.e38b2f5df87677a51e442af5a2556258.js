352fd4931d5c3be21d1c426f6b4931d32c2574b5
RussianStupidCode <67269808+RussianStupidCode@users.noreply.github.com>
2022-07-11 07:31:13 +0000

    Deploying to gh-pages from @ RussianStupidCode/cart-react@471b4ea8f8e5664a84f9176251433a0b39ca652b 🚀

---
+++ precache-manifest.e38b2f5df87677a51e442af5a2556258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28c7886e6c7c74111f76",
+    "url": "/cart-react/static/css/main.40f58a8f.chunk.css"
+  },
+  {
+    "revision": "28c7886e6c7c74111f76",
+    "url": "/cart-react/static/js/main.28c7886e.chunk.js"
+  },
+  {
+    "revision": "4df21fd74f3382540de2",
+    "url": "/cart-react/static/css/1.885d8fed.chunk.css"
+  },
+  {
+    "revision": "4df21fd74f3382540de2",
+    "url": "/cart-react/static/js/1.4df21fd7.chunk.js"
+  },
+  {
+    "revision": "0e62c9dd09925e32fc11",
+    "url": "/cart-react/static/js/runtime~main.0e62c9dd.js"
+  },
+  {
+    "revision": "89e502b6922189387b9852cfcc76d336",
+    "url": "/cart-react/index.html"
+  }
+];
